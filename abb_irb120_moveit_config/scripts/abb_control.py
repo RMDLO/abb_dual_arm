e25e52c81c1c999d7f75e5b440fc020915218c8d
JeffersonAero <holly.dinkel@gmail.com>
2023-05-22 18:48:51 -0500

    add dual-arm control using joint angles in abb_control.py script

--- abb_irb120_moveit_config/scripts/abb_control.py
+++ abb_irb120_moveit_config/scripts/abb_control.py
@@ -3,6 +3,7 @@
 import rospy
 import moveit_commander
 from std_msgs.msg import Header
+from moveit_msgs.msg import DisplayTrajectory
 from control_msgs.msg import FollowJointTrajectoryActionGoal
 from numpy import pi
 
@@ -10,61 +11,48 @@ def functional():
 
     rospy.init_node('dual_arm_planning')
 
-    # Initialize the robot commander for each robot arm
-    robot1 = moveit_commander.RobotCommander(ns="mp")
-    robot2 = moveit_commander.RobotCommander(ns="mp_m")
-    scene = moveit_commander.PlanningSceneInterface()
-    
-    # Create a move group commander for each robot arm
-    group1 = moveit_commander.MoveGroupCommander("mp")
-    group2 = moveit_commander.MoveGroupCommander("mp_m")
-    group1.set_start_state_to_current_state()
-    group2.set_start_state_to_current_state()
+    # Create a move group commander
+    group= moveit_commander.MoveGroupCommander("dual_arm")
+    group.set_start_state_to_current_state()
 
-    pub_rob1 = rospy.Publisher('/mp/joint_trajectory_action/goal', FollowJointTrajectoryActionGoal, queue_size=1)
-    pub_rob2 = rospy.Publisher('/mp_m/joint_trajectory_action/goal', FollowJointTrajectoryActionGoal, queue_size=1)
+    pub = rospy.Publisher('/dual_arm/joint_trajectory_action/goal', FollowJointTrajectoryActionGoal, queue_size=1)
+    display_pub = rospy.Publisher('/move_group/display_planned_path', DisplayTrajectory, queue_size=10)
 
-    joint_goal1 = group1.get_current_joint_values()
-    joint_goal2 = group2.get_current_joint_values()
+    joint_goal = group.get_current_joint_values()
+
+    message = FollowJointTrajectoryActionGoal()
+    display_trajectory = DisplayTrajectory()
+    display_trajectory.trajectory_start = (joint_goal)
 
     # Set the joint goal for each robot arm
-    joint_goal1 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]  # Joint values for arm1
-    joint_goal2 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]  # Joint values for arm2
+    joint_goal = [pi/4, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 0.0, pi/4, 0.0, 0.0, 0.0]
+    joints = ['joint_2', 'joint_3', 'joint_4', 'joint_5', \
+                'joint_6', 'joint_7', 'joint_2_m', 'joint_3_m', \
+                'joint_4_m', 'joint_5_m', 'joint_6_m', 'joint_7_m']
 
-    message_rob1 = FollowJointTrajectoryActionGoal()
-    message_rob2 = FollowJointTrajectoryActionGoal()
+    message.goal.trajectory.joint_names = joints
+    display_trajectory.model_id = 'dual_arm'
 
-    joint_names = ['joint_2', 'joint_3', 'joint_4', 'joint_5', \
-                  'joint_6', 'joint_7']
-    joint_names_m = ['joint_2_m', 'joint_3_m', 'joint_4_m', 'joint_5_m', \
-                  'joint_6_m', 'joint_7_m']
-    message_rob1.goal.trajectory.joint_names = joint_names
-    message_rob2.goal.trajectory.joint_names = joint_names_m
+    group.set_joint_value_target(joint_goal)
 
-    group1.set_joint_value_target(joint_goal1)
-    group2.set_joint_value_target(joint_goal2)
+    plan = group.plan()
 
-    plan1 = group1.plan()
-    plan2 = group2.plan()
+    traj = plan[1]
 
-    traj1 = plan1[1]
-    traj2 = plan2[1]
+    message.goal.trajectory = traj.joint_trajectory
+    display_trajectory.trajectory = traj.joint_trajectory
 
-    message_rob1.goal.trajectory = traj1.joint_trajectory
-    message_rob2.goal.trajectory = traj2.joint_trajectory
-
-    header_rob1 = Header()
-    header_rob1.stamp = rospy.Time.now()
-    message_rob1.goal.trajectory.header = header_rob1
-
-    header_rob2 = Header()
-    header_rob2.stamp = rospy.Time.now()
-    message_rob2.goal.trajectory.header = header_rob2
+    header = Header()
+    header.stamp = rospy.Time.now()
+    message.goal.trajectory.header = header
 
     #  publishing to ROS node
-    pub_rob1.publish(message_rob1)
-    pub_rob2.publish(message_rob2)
-
+    pub.publish(message)
+    display_pub.publish(display_trajectory)
+    execution = group.go(wait=True)
+    print(execution)
+    group.stop()
+    group.clear_pose_targets()
 
 if __name__ == '__main__':
     try:
