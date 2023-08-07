f2e49e5c9ab1fd54d3806fe6bdb7d813f5f339c1
hollydinkel <holly.dinkel@gmail.com>
2023-08-07 16:27:32 -0500

    [WIP] add abb_pose_control.py for cartesian control of one arm

--- abb_control/scripts/abb_pose_control.py
+++ abb_control/scripts/abb_pose_control.py
@@ -1,35 +1,46 @@
 #!/usr/bin/env python3
 
-from numpy import pi
-
 import rospy
 import moveit_commander
-from std_msgs.msg import Header
 from sensor_msgs.msg import JointState
 from moveit_msgs.msg import DisplayTrajectory
 from control_msgs.msg import FollowJointTrajectoryActionGoal
+from geometry_msgs.msg import Pose, Quaternion
 
 def functional():
 
-    rospy.init_node('dual_arm_joint_planning')
-    
-    group= moveit_commander.MoveGroupCommander("dual_arm")
+    rospy.init_node('dual_arm_cartesian_planning')
+    group_name = "mp_m"
+    group= moveit_commander.MoveGroupCommander(group_name)
     group.set_start_state_to_current_state()
 
     pub = rospy.Publisher('/dual_arm/joint_trajectory_action/goal', FollowJointTrajectoryActionGoal, queue_size=1)
     display_pub = rospy.Publisher('/move_group/display_planned_path', DisplayTrajectory, queue_size=10)
 
     joint_start = group.get_current_joint_values()
-    print("Starting joint values: ", joint_start)
-    joint_goal = [0, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 0.0, pi/4, 0.0, 0.0, 0.0]
-    print("Goal joint values: ", joint_goal)
+    start_pose = group.get_current_pose().pose
+    print("Start pose: ", start_pose)
 
-    joint_names = ['joint_2', 'joint_3', 'joint_4', 'joint_5', \
-                'joint_6', 'joint_7', 'joint_2_m', 'joint_3_m', \
-                'joint_4_m', 'joint_5_m', 'joint_6_m', 'joint_7_m']
-    
-    group.set_joint_value_target(joint_goal)
-    _, plan, _, _ = group.plan()
+    goal_pose = Pose()
+    goal_pose.position.x = 0.7
+    goal_pose.position.y = 0
+    goal_pose.position.z = 0.3
+    goal_pose.orientation = Quaternion(0, 0, 0, 1)
+    print("Goal pose: ", goal_pose)
+
+    waypoints = []
+    waypoints.append(start_pose)
+    waypoints.append(goal_pose)
+
+    (plan, _) = group.compute_cartesian_path(waypoints, 0.01, 0.0)  # 0.01 is the step size, 0.0 is the jump threshold
+
+    if group_name == "dual_arm":
+        joint_names = ['joint_2', 'joint_3', 'joint_4', 'joint_5', \
+                    'joint_6', 'joint_7', 'joint_2_m', 'joint_3_m', \
+                    'joint_4_m', 'joint_5_m', 'joint_6_m', 'joint_7_m']
+    elif group_name == "mp_m":
+        joint_names = ['joint_2_m', 'joint_3_m', 'joint_4_m', \
+                       'joint_5_m', 'joint_6_m', 'joint_7_m']
     
     joint_state = JointState()
     joint_state.header.stamp = rospy.Time.now()
@@ -37,7 +48,7 @@ def functional():
     joint_state.position = joint_start
 
     display_trajectory = DisplayTrajectory()
-    display_trajectory.model_id = 'dual_arm'
+    display_trajectory.model_id = group_name
     display_trajectory.trajectory.append(plan)
     display_trajectory.trajectory_start.joint_state = joint_state
 
