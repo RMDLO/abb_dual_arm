618a817bd3b362fb64a28873861d9fde75dd1bd0
Raghav Navaratna <rsnavaratna@gmail.com>
2023-10-21 22:20:01 -0500

    [UPDATE] added some safety features and documentation to pose control node

--- abb_irb120_support/src/abb_pose_control_node.cpp
+++ abb_irb120_support/src/abb_pose_control_node.cpp
@@ -1,3 +1,31 @@
+/**
+ * @file   abb_irb120_cartesian_planning.cpp
+ * @brief  This ROS node performs Cartesian path planning for an ABB IRB 120 industrial robot.
+ *         The program uses MoveIt to control the robot and TF2 for coordinate transformations.
+ *
+ * Required ROS Libraries:
+ * - ros/ros.h: ROS core library for C++.
+ * - moveit/move_group_interface/move_group_interface.h: MoveIt interface for controlling the robot.
+ * - tf2_ros/transform_listener.h: TF2 library to listen for coordinate transforms.
+ * - tf2_geometry_msgs/tf2_geometry_msgs.h: TF2 library for geometry messages.
+ * - sensor_msgs/JointState.h: ROS standard message type for reporting joint states.
+ * - moveit_msgs/DisplayTrajectory.h: Message to display robot trajectory in Rviz.
+ * - control_msgs/FollowJointTrajectoryActionGoal.h: Message to send joint trajectory goals.
+ * - geometry_msgs/PoseStamped.h: Message to hold pose data with frame information.
+ *
+ * Workflow:
+ * 1. Initialize ROS and create a node handle.
+ * 2. Initialize MoveIt for a given planning group.
+ * 3. Define ROS publishers for sending trajectory goals and visualizing the planned path.
+ * 4. Set up TF2 to listen to transforms between frames.
+ * 5. Create a vector to hold waypoints for the Cartesian path.
+ * 6. Use TF2 to transform the target pose (marker frame) to the robot's planning frame.
+ * 7. Compute the Cartesian path using MoveIt and get the fraction of the path that was successfully computed.
+ * 8. If the path is viable (fraction >= 0.9), execute the plan. Otherwise, print a warning.
+ *
+ * Note: Replace the 'jump_threshold' and other parameters according to your application needs.
+ */
+
 #include <ros/ros.h>
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <tf2_ros/transform_listener.h>
@@ -53,9 +81,9 @@ int main(int argc, char** argv)
 
         moveit::planning_interface::MoveGroupInterface::Plan plan;
  
-        double fraction = group.computeCartesianPath(waypoints, 0.01, 0.0, plan.trajectory_, true);
+        double fraction = group.computeCartesianPath(waypoints, 0.01, 0.5, plan.trajectory_, true);
     
-        // Add this snippet here to check the fraction returned
+        // To check the fraction returned
         if (fraction >= 0.9)  
         {
             // Populate action goal and other steps before execution
